ad28834b961c54bc11878d85e8d3d3c0a882385a
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-05 12:42:09 -0500

    first commit

---
+++ build/precache-manifest.d595924c5cc8cc8e37ccf054340b594e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df5bebb920293601761bb6584f3e9c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "550ae0b10fa9a4b6afb9",
+    "url": "/static/css/main.52689709.chunk.css"
+  },
+  {
+    "revision": "0392a618e55a6705606d",
+    "url": "/static/js/2.e5615630.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e5615630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550ae0b10fa9a4b6afb9",
+    "url": "/static/js/main.039f039f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.039f039f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c56e041857a0bf600d3",
+    "url": "/static/js/runtime-main.3715f0a4.js"
+  }
+]);
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from './header';
+export { default as UserPosts } from './UserPosts';
+export { default as UserTodos } from './UserTodos';
